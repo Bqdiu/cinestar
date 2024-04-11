836fcd15d66b2c088b6d7214a8f5a87f5b09f9e3
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-04-11 23:16:38 +0700

    okkk

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layout')
 
 @section('main-content')
+<div class="container">
+   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/img/biet-doi-san-ma.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/img/thanh-xuan-18x2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/img/civil-war.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+   </div>
 @endsection
