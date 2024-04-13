ec82facde47252a6649e98bc222dbce22a095834
dungry853 <129316318+dungry853@users.noreply.github.com>
2024-04-14 01:22:05 +0700

    UpdateDetail

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,13 +7,16 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     public function Index(){
-        $Phim = DB::select('select * from movie');
+        $Phim = DB::select('select movie.*,age_regulation.* from movie,age_regulation where movie.RegulationID = age_regulation.RegulationID');
         $Status = DB::select('select * from status_movie');
         return view('client/home/index',compact('Status','Phim'));
     }
     public function DetailProduct($id){
-        $PhimItem = DB::select('select * from movie where MovieID = ?',[$id]);
-        return view('client/home/detailproduct',compact('PhimItem'));
+        $PhimItem = DB::table('movie')->join('age_regulation','movie.RegulationID','=','age_regulation.RegulationID')
+                        ->select('movie.*','age_regulation.*')
+                        ->where('movie.MovieID',$id)
+                        ->first();
+        return view('client/home/detailproduct',['PhimItem' => $PhimItem]);
     }
     public function Login(){
         return view('client/user/login');
--- resources/views/client/home/detailproduct.blade.php
+++ resources/views/client/home/detailproduct.blade.php
@@ -1,66 +1,68 @@
 @extends ('layout');
 
 @section('main-content')
+
+
 <div class="container mt-3">
     <div class="row">
         <div class="col-md-5 d-flex justify-content-center" style="position:relative">
             <div class="type-movie-box">
                 <div class="type-movie"><span class="txt">2D</span></div>
-                <div class="age"><span class="num">T13</span><span class="txt">KID</span></div>
+                <div class="age"><span class="num">{{$PhimItem->AgeRegulationName}}</span><span class="txt">{{$PhimItem->Object}}</span></div>
             </div>
       
-        <img style="width:85%;border-radius:5px" src="https://cinestar.com.vn/_next/image/?url=https%3A%2F%2Fapi-website.cinestar.com.vn%2Fmedia%2Fwysiwyg%2FPosters%2F04-2024%2Fthanh-xuan-18x2-poster-2.jpg&w=1920&q=75" alt="">
+        <img style="width:85%;border-radius:5px" src="/imgMovie/{{$PhimItem->Thumbnail}}" alt="">
         </div>
         <div class="col-md-7">
-        <h2 class="text-white" style="font-weight:1000;margin-bottom:20px" >THANH XUÂN 18X2: LỮ TRÌNH HƯỚNG VỀ EM (T13)</h2>
+        <h2 class="text-white" style="font-weight:1000;margin-bottom:20px" >{{$PhimItem->Title}}</h2>
        <ul class="info-detail">
         <li>
             <span><img class="span-icon" src="/img/icon-tag.svg" alt=""></span>
-            <span class="text-white">Tình cảm</span>
+            <span class="text-white">{{$PhimItem->Genre}}</span>
         </li>
         <li>
             <span><img class="span-icon" src="/img/icon-clock.svg" alt=""></span>
-            <span class="text-white">135'</span>
+            <span class="text-white">{{$PhimItem->Duration}}</span>
         </li>
 
         <li>
             <span class="span-icon" style="color:yellow"><i class="fas fa-globe-asia" style="padding-left:4px"></i></span>
-            <span class="text-white">Khác</span>
+            <span class="text-white">{{$PhimItem->Language}}</span>
         </li>
         <li>
             <span class="span-icon"><img style="width:24px;height:24px" src="/img/subtitle.svg" alt=""></span>
-            <span class="text-white">Phụ Đề</span>
+            <span class="text-white">{{$PhimItem->Country}}</span>
         </li>
         <li>
             <span class="span-icon"><i  class="fas fa-user-check" style="color:yellow;padding-left:4px"></i></span>
-            <span class="text-black" style="background:yellow">T13:Phim dành cho khán giả từ đủ 13 tuổi trở lên</span>
+            <span class="text-black" style="background:yellow">{{$PhimItem->Content}}</span>
         </li>
        </ul>
        <h4 class="text-white" style="font-weight:1000;margin-top:20px">Mô Tả</h4>
        <ul class="info-detail text-white" style="font-size:14px">
         <li>
             <span>Đạo diễn:</span>
-            <span>Michihito Fujii</span>
+            <span>{{$PhimItem->Director}}</span>
         </li>
         <li>
             <span>Diễn viên:</span>
-            <span>Hứa Quang Hán, Kiyohara Kaya, Trương Chấn</span>
+            <span>{{$PhimItem->Actor}}</span>
         </li>
         <li>
             <span>Khởi chiếu:</span>
-            <span>03/04/2024</span>
+            <span>{{$PhimItem->ReleaseDate}}</span>
         </li>
        </ul>
        <h4 class="text-white" style="font-weight:1000;margin-top:3.2rem;">NỘI DUNG PHIM</h4>
-       <p class="text-white" style="width:85%">Ký ức tình đầu ùa về khi Jimmy nhận được tấm bưu thiếp từ Ami. Cậu quyết định một mình bước lên chuyến tàu đến Nhật Bản gặp lại người con gái cậu đã bỏ lỡ 18 năm trước. Mối tình day dứt thời thanh xuân, liệu sẽ có một kết cục nào tốt đẹp khi đoàn tụ?</p>
+       <p class="text-white" style="width:85%">{{$PhimItem->Description}}</p>
         
        <div class="trailer-link" style="margin-top:3.2rem">
-       <a href="" class="w-100 text-center link-text-btn" style="margin:auto;font-size:20px;border:1px solid yellow;padding:20px;border-radius:10px;color:yellow"><img style="width:37px;height:37px" src="https://cinestar.com.vn/assets/images/icon-play-vid.svg" alt=""> Xem trailer</a>
+       <a href="{{$PhimItem->trailer_url}}" class="w-100 text-center link-text-btn" style="margin:auto;font-size:20px;border:1px solid yellow;padding:20px;border-radius:10px;color:yellow"><img style="width:37px;height:37px" src="https://cinestar.com.vn/assets/images/icon-play-vid.svg" alt=""> Xem trailer</a>
        </div>
         </div>
     </div>
 </div>
 <div class="container">
 <h2 class="text-white text-center" style="font-weight:1000;margin-bottom:20px;margin-top:100px" >LỊCH CHIẾU</h2>
-</div>
+
 @endsection
